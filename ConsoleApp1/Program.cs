4a0d4c8ccbdd967f9348037c20d2d8224acd5406
Anton Belov <us5nar@gmail.com>
2018-08-31 18:00:23 +0300

    a few experiments with arguments and paramentres

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,15 +13,16 @@ namespace ConsoleApp1
             Person joe = new Person();
             Person vasilij = new Person("Вася");
             Person james = new Person("James Bond", 33);
-            //Person adult = new Person("Big Guy", 39, "spy");
+            Person adult = new Person("Big Guy", "spy");
             //it is an example of private constructor as a base
-            Person tom = new Person { name = "Tom", age = 31, position = "Chupacabra" };//init of an object - overridding all constructors
+            //Works only if fields are public
+            //Person tom = new Person { _name = "Tom", _age = 31, _position = "Chupacabra" };//init of an object - overridding all constructors
 
             joe.SayHello();
             vasilij.SayHello();
             james.SayHello();
-            //adult.SayHello();
-            tom.SayHello();
+            adult.SayHello();
+            //tom.SayHello();
 
             Console.ReadLine();
         }
